ca834b97c1d63c547f9472f07947545d9e60507f
ud17 <upandya1297@gmail.com>
2022-08-10 12:18:06 -0400

    todo: error ref added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,29 +3,44 @@ import "../styles/Form.css";
 import Card from "./Card";
 
 // import react hooks
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Form = () => {
 
     const [text, setText] = useState([]);
     const [input, setInput] = useState("");
+    const [error, setError] = useState("");
+
+    const inputRef = useRef(null);
 
     const addTodo = () => {
+        if(input.length < 4) {
+            setError("Task title should be 4-30 characters long.");
+            return;
+        }
         text.push(input);
         setText(text);
         setInput("");
+        onSubmit();
     }
 
     const setValue = (event) => {
         setInput(event.target.value);
+        setError("");
+    }
+
+    const onSubmit = () => {
+        inputRef.current.value = "";
+        inputRef.current.blur();
     }
 
     return (
         <div>
             <form className="form-container">
-                <input type="input" className="input margin border" onChange={setValue} placeholder="Todo Task" maxLength={30}/>
+                <input type="input" className="input margin border" onChange={setValue} ref={inputRef} placeholder="Todo Task" maxLength={30} minLength={4}/>
                 <input type="button" className="button margin border" value="Add Todo" onClick={addTodo} />
             </form>
+            <label className="error">{error}</label>
             <div className="div-container">
                 {
                     text.map((item) => (
